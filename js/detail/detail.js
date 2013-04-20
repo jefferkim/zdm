770b4be7694c256924fc1c1998cf8a57e37e011e
jinjianfeng <wb-jinjianfeng@taobao.com>
2013-04-21 03:15:12 +0800

    edit

--- js/detail/detail.js
+++ js/detail/detail.js
@@ -186,13 +186,33 @@
 
         queryComment:function (ratedId) {
 
+
+
+
             var self = this;
-            var data = {"ratedUid":ratedId, "tradeId":"0", "itemIds":this.itemQid, "pageSize":"5", "pageIndex":"1"};
+            var data = {"ratedUid":ratedId, "tradeId":"0", "itemIds":this.itemQid, "pageSize":"100", "pageIndex":"1"};
+
+            var filterComments = function(comments){
+                var commentsSet = comments;
+                var tmp = [];
+                _.each(commentsSet,function(comment){
+                    if(comment.feedItemPicDOList.length >= 1 && tmp.length <=5){
+                        tmp.push(comment);
+                    }
+                });
+                if(tmp.length <5){
+                    //tmp.push(commentsSet)
+                }
+
+            }
 
             app.mtopH5Api.getApi('mtop.gene.feedCenter.queryFeedItems', '1.0', data, {}, function (result) {
 
                 if (result.ret && result.ret[0] == 'SUCCESS::调用成功' && result.data) {
-                    var comments = result.data.dataList;
+                    var comments = self.comments = result.data.dataList;
+
+                    filterComments(comments);
+
 
                     var html = self.templates['comments']({comments:comments});
 
